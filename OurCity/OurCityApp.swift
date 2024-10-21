24c8a02b4557391acdf676df67efc035a4f54f8e
Chingis <Kekwaker@gmail.com>
2024-10-21 21:26:46 +0800

    Initial Commit

---
+++ OurCity/OurCityApp.swift
@@ -0,0 +1,17 @@
+//
+//  OurCityApp.swift
+//  OurCity
+//
+//  Created by Qin Chingis on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct OurCityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
