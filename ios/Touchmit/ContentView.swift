6045cf244c38bc2d588daa9d3fb4290e82874784
Chingis <Kekwaker@gmail.com>
2024-10-26 00:19:18 +0800

    asdf

--- ios/Touchmit/ContentView.swift
+++ ios/Touchmit/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("hello, world!")
+            Text("sadffs, world!")
         }
         .padding()
     }
