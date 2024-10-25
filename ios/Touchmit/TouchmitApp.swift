a799e1cf7e37749abfdb99352e0b734d9fbf9516
Chingis <Kekwaker@gmail.com>
2024-10-26 00:12:51 +0800

    asd

--- ios/Touchmit/TouchmitApp.swift
+++ ios/Touchmit/TouchmitApp.swift
@@ -7,34 +7,6 @@
 
 import SwiftUI
 import FirebaseCore
-import FirebaseFirestore
-// Функция для добавления пользователя в Firestore
-func addUserToFirestore(userId: String, passwordHash: String) {
-    // Ссылка на Firestore
-    let db = Firestore.firestore()
-    
-    // Коллекция "User", куда мы добавим данные
-    let userCollection = db.collection("User")
-    
-    // Данные для нового документа
-    let newUser: [String: Any] = [
-        "userId": userId,
-        "passwordHash": passwordHash
-    ]
-    
-    // Добавляем документ с указанным userId как идентификатором документа
-    userCollection.document(userId).setData(newUser) { error in
-        if let error = error {
-            print("Ошибка при добавлении пользователя: \(error.localizedDescription)")
-        } else {
-            print("Пользователь успешно добавлен в Firestore!")
-        }
-    }
-}
-
-// Пример вызова функции
-addUserToFirestore(userId: "user123", passwordHash: "hashed_password")
-
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
